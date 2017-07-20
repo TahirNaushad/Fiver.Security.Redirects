068018d1c30de25da579a10cff675a1018e3fb8b
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-20 09:45:21 +0100

    redirect attacks

---
+++ Fiver.Security.Redirects/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiver.Security.Redirects.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => Content("Home");
+        public IActionResult About() => Content("About");
+        public IActionResult Exception() => Content("Exception");
+        public IActionResult Error() => Content("Error");
+
+        public IActionResult GoLocalRedirect(string url) => LocalRedirect(url);
+
+        public IActionResult GoIsLocalUrl(string url)
+        {
+            if (Url.IsLocalUrl(url))
+                return Redirect(url); 
+            else
+                return RedirectToAction("Error", "Home");
+        }
+    }
+}
